fd7e6931e0777609bd9319c6b2b962b54f927e9f
Ahmed Ibrahim <a.ibrahim@itqansystems.com>
2020-10-01 06:49:38 +0200

    before last build

--- mohamed ashrsf/javascript/login.js
+++ mohamed ashrsf/javascript/login.js
@@ -8,23 +8,27 @@ function setAction(form)
     if (this.readyState == 4 && this.status == 200)
         {
         var UserData = JSON.parse(xmlhttp.responseText);
-        for(i = 0; i < UserData.UserData.length; i++)
+        for(i = 0; i < UserData.length; i++)
         {
            
-           if(user_name==UserData.UserData[i].name && password_user==UserData.UserData[i].password)
+           if(user_name==UserData[i].name && password_user==UserData[i].password)
                {
                   console.log('user name '+user_name)
                   console.log('passwoed' + password_user) 
                   console.log('sueccssful')
                   alert('تم التاكد من الاسم والباسورد تفضل للدخول')
-                  if(UserData.UserData[i].job==null)  
-                    {
-                        window.location.replace("../html/homepage.htm")
+                  sessionStorage.removeItem('currenLoginUser')
+                  sessionStorage.setItem('currenLoginUser', UserData[i].name)
+                  if(UserData[i].job=="none")  
+                    {// if customer
+                        // document.getElementsByName("myform").setAttribute("action", "/mohamed abdelnaeem/home_customer.html")
+                        window.location.replace("/mohamed abdelnaeem/home_customer.html")
                         noSigIn=true
                     }
                   else
-                    {
-                        window.location.replace("../html/Login.htm") 
+                    {// if tech
+                        // document.getElementsByName("myform").setAttribute("action", "/ahmed ibrahim/tech-orders-view.html")
+                        window.location.replace("/ahmed ibrahim/tech-orders-view.html") 
                         noSigIn=true
                     }
                   break
@@ -40,10 +44,10 @@ function setAction(form)
 
         }
     };
-    xmlhttp.open("GET", "../jsonfile/userData.json", true);
+    xmlhttp.open("GET", "http://m0stafaeladawy00.pythonanywhere.com/UsersAccounts/", true);
     xmlhttp.send();
 }
 
 function goSignUp(){
-    window.location.replace("../html/homepage.htm")   
+    window.location.replace("/mostafa/Html/register.html")   
 }
